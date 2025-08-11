be18ddecc118f4dc883232e142d65d4a64d860cc
Tomoki KANDA <t23cs014@yamanashi.ac.jp>
2025-08-11 21:03:29 +0900

    chmod を一括で行うスクリプトの作成。

---
+++ chmodScript.sh
@@ -0,0 +1,14 @@
+# htmlファイルの権限
+chmod 744 index.html
+
+# cssファイルの権限
+chmod 711 css
+chmod 744 ./css/style.css
+
+# jsファイルの権限
+chmod 711 js
+chmod 744 ./js/script.js
+
+# phpファイルの権限
+chmod 711 php
+chmod 744 ./php/*.php
